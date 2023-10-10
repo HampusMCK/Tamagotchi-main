9f186d9e81ba3a33b9ab2f8cdc5debeaf1df8c13
Hampus Eriksson <Hampus.Eriksson@elev.ga.ntig.se>
2023-10-10 17:30:45 +0200

    save bug fix

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -1,20 +1,20 @@
 ﻿Console.Title = "TamaGucci";
 
+Random gen = new();
 Store ica = new Store();
-List<Tamagotchi> kidList = new();
 Tamagotchi kid = new();
 bool isinStore = false;
 string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 string filePath = Path.Combine(path, "saveFile.txt");
 List<Action> eventList = new()
     {
-    kid.Teach,
-    kid.Hi,
-    kid.Feed,
+    Teach,
+    Hi,
+    Feed,
     GoToStore,
     checkInventory,
-    kid.DressUp,
-    kid.Exhibition
+    DressUp,
+    Exhibition
     };
 
 if (File.Exists(filePath))
@@ -33,6 +33,7 @@ if (kid.name == null)
     Console.WriteLine("Name Your TamaGucci!");
     kid.name = Console.ReadLine();
 }
+
 ica.addItems();
 while (kid.isAlive)
 {
@@ -52,11 +53,12 @@ while (kid.isAlive)
             if (answerInt == i + 1)
             {
                 eventList[i]();
+                saveGame();
             }
         }
-        kid.Tick();
-        Console.ReadLine();
+        Tick();
         saveGame();
+        Console.ReadLine();
     }
 
     while (kid.isAlive && isinStore)
@@ -87,10 +89,13 @@ Console.ReadLine();
 
 
 
-
 void saveGame()
 {
-    var serializedObject = Newtonsoft.Json.JsonConvert.SerializeObject(kid, Newtonsoft.Json.Formatting.Indented);
+    var JsonSetting = new Newtonsoft.Json.JsonSerializerSettings
+    {
+        TypeNameHandling = Newtonsoft.Json.TypeNameHandling.All
+    };
+    var serializedObject = Newtonsoft.Json.JsonConvert.SerializeObject(kid, Newtonsoft.Json.Formatting.Indented, JsonSetting);
 
 
     using (StreamWriter sw = new StreamWriter(filePath))
@@ -110,13 +115,17 @@ void saveGame()
 
 void loadGame()
 {
+    var JsonSetting = new Newtonsoft.Json.JsonSerializerSettings
+    {
+        TypeNameHandling = Newtonsoft.Json.TypeNameHandling.All
+    };
     string content = null;
     using (StreamReader sr = new StreamReader(filePath))
     {
         content = sr.ReadToEnd();
     }
 
-    var kidReturned = Newtonsoft.Json.JsonConvert.DeserializeObject<Tamagotchi>(content);
+    var kidReturned = Newtonsoft.Json.JsonConvert.DeserializeObject<Tamagotchi>(content, JsonSetting);
     kid = kidReturned;
 }
 
@@ -211,3 +220,227 @@ void checkInventory()
         Console.WriteLine($"{kid.ownedFood[i].ItemName}");
     }
 }
+
+
+ void Feed()
+    {
+        Console.WriteLine($"What would you like to feed {kid.name} with?");
+        for (int i = 0; i < kid.ownedFood.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {kid.ownedFood[i].ItemName}, Hunger Decrease: {kid.ownedFood[i].HungerDecrease}");
+        }
+        string FeedChoise = Console.ReadLine();
+        int FC;
+        int.TryParse(FeedChoise, out FC);
+        for (int i = 0; i < kid.ownedFood.Count; i++)
+        {
+            if (FC == i + 1)
+            {
+                kid.hunger -= kid.ownedFood[i].HungerDecrease;
+                kid.ownedFood.RemoveAt(i);
+            }
+        }
+        if (kid.hunger < 0)
+        {
+            kid.hunger = 0;
+        }
+    }
+
+     void Hi()
+    {
+        int choise = gen.Next(kid.words.Count);
+        if (kid.words.Count > 0)
+        {
+            Console.WriteLine($"{kid.name} said: {kid.words[choise]}");
+        }
+        else
+        {
+            Console.WriteLine($"{kid.name} doesn't know any words, you have to teach them some!(press enter to continue)");
+            Console.ReadLine();
+            Teach();
+        }
+        ReduceBoredom();
+    }
+
+    void Teach()
+    {
+        Console.Clear();
+        Console.WriteLine($"What would you like to teach {kid.name}?");
+        string newWord = Console.ReadLine();
+        kid.words.Add(newWord);
+        Console.WriteLine($"You taught {kid.name} to say '{newWord}'");
+        ReduceBoredom();
+    }
+
+   void Tick()
+    {
+        kid.boredom += gen.Next(10);
+        kid.hunger += gen.Next(7);
+        if (kid.hunger >= 100 || kid.boredom >= 200)
+        {
+            kid.isAlive = false;
+        }
+        PrintStats();
+    }
+
+    void PrintStats()
+    {
+        string aliveTrue;
+        if (kid.isAlive)
+        {
+            aliveTrue = "alive";
+        }
+        else
+        {
+            aliveTrue = "dead";
+        }
+        Console.WriteLine($"Hunger: {kid.hunger}/100, Boredom: {kid.boredom}/200, you have ${kid.money}, {kid.name} is {aliveTrue}");
+    }
+
+    bool GetAlive()
+    {
+        if (kid.isAlive)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void ReduceBoredom()
+    {
+        kid.boredom = kid.boredom / 2;
+    }
+
+    void DressUp()
+    {
+        Console.Clear();
+        Console.WriteLine($"what would you like to dress {kid.name} in? {kid.name} is currently wearing {kid.dressed.Count} items");
+        for (int i = 0; i < kid.ownedItems.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {kid.ownedItems[i].ItemName}, Exhibition Score: {kid.ownedItems[i].ExhibitionScore}");
+        }
+        string dressChoise = Console.ReadLine();
+        int DC;
+        int.TryParse(dressChoise, out DC);
+        for (int i = 0; i < kid.ownedItems.Count; i++)
+        {
+            if (DC == i + 1)
+            {
+                kid.dressed.Add(kid.ownedItems[i]);
+                kid.ownedItems.RemoveAt(i);
+            }
+        }
+        if (kid.dressed.Count > 3)
+        {
+            Console.WriteLine($"{kid.name} is wearing to many things! {kid.dressed[0].ItemName} got taken off! press enter to continue!");
+            kid.ownedItems.Add(kid.dressed[0]);
+            kid.dressed.RemoveAt(0);
+            Console.ReadLine();
+        }
+        Console.WriteLine($"{kid.name} is currently wearing:");
+        for (int i = 0; i < kid.dressed.Count; i++)
+        {
+            Console.WriteLine($"{kid.dressed[i].ItemName}, Exh.points {kid.dressed[i].ExhibitionScore}");
+        }
+        ReduceBoredom();
+    }
+
+    void setExScore()
+    {
+        kid.ExScore = 5;
+        for (int i = 0; i < kid.dressed.Count; i++)
+        {
+            kid.ExScore += kid.dressed[i].ExhibitionScore;
+        }
+    }
+
+    void Exhibition()
+    {
+        Console.Clear();
+        setExScore();
+        int win;
+        int diff;
+        Console.WriteLine($"At What Grade Would You Like to Compete In? 1 (Avg. Exh.points: 15, 1st price: $30),\n2 (AVg. Exh.points: 30, 1st price: $50),\nor 3 (Avg. Exh.points: 60, 1st price: $100),\n{kid.name} has {kid.ExScore} in Exh.ponits");
+        string DiffChoise = Console.ReadLine();
+        int DiC;
+        int.TryParse(DiffChoise, out DiC);
+        if (DiC == 1)
+        {
+            kid.e.AddOps1();
+            diff = kid.e.op1[0].ExP + kid.e.op1[1].ExP + kid.e.op1[0].ExP + kid.ExScore;
+            win = gen.Next(diff);
+            win -= kid.ExScore;
+            if (win <= kid.ExScore)
+            {
+                Console.WriteLine("Congratulations! You won $30");
+                kid.money += 30;
+            }
+            else if (win >= kid.ExScore && win <= kid.ExScore + kid.e.op1[0].ExP)
+            {
+                Console.WriteLine($"You Lost to {kid.e.op1[0].name} );");
+            }
+            else if (win >= kid.ExScore + kid.e.op1[0].ExP && win <= kid.ExScore + kid.e.op1[0].ExP + kid.e.op1[1].ExP)
+            {
+                Console.WriteLine($"You Lost to {kid.e.op1[1].name} );");
+            }
+            else
+            {
+                Console.WriteLine($"You Lost to {kid.e.op1[2].name} );");
+            }
+            kid.e.op1.RemoveRange(0, 3);
+        }
+        else if (DiC == 2)
+        {
+            kid.e.AddOps2();
+            diff = kid.e.op2[0].ExP + kid.e.op2[1].ExP + kid.e.op2[0].ExP + kid.ExScore;
+            win = gen.Next(diff);
+            win -= kid.ExScore;
+            if (win <= kid.ExScore)
+            {
+                Console.WriteLine("Congratulations! You won $50");
+                kid.money += 50;
+            }
+            else if (win >= kid.ExScore && win <= kid.ExScore + kid.e.op2[0].ExP)
+            {
+                Console.WriteLine($"You Lost to {kid.e.op2[0].name} );");
+            }
+            else if (win >= kid.ExScore + kid.e.op2[0].ExP && win <= kid.ExScore + kid.e.op2[0].ExP + kid.e.op2[1].ExP)
+            {
+                Console.WriteLine($"You Lost to {kid.e.op2[1].name} );");
+            }
+            else
+            {
+                Console.WriteLine($"You Lost to {kid.e.op2[2].name} );");
+            }
+            kid.e.op2.RemoveRange(0, 3);
+        }
+        else if (DiC == 3)
+        {
+            kid.e.AddOps3();
+            diff = kid.e.op3[0].ExP + kid.e.op3[1].ExP + kid.e.op3[0].ExP + kid.ExScore;
+            win = gen.Next(diff);
+            win -= kid.ExScore;
+            if (win <= kid.ExScore)
+            {
+                Console.WriteLine("Congratulations! You won $100");
+                kid.money += 100;
+            }
+            else if (win >= kid.ExScore && win <= kid.ExScore + kid.e.op3[0].ExP)
+            {
+                Console.WriteLine($"You Lost to {kid.e.op3[0].name} );");
+            }
+            else if (win >= kid.ExScore + kid.e.op3[0].ExP && win <= kid.ExScore + kid.e.op3[0].ExP + kid.e.op3[1].ExP)
+            {
+                Console.WriteLine($"You Lost to {kid.e.op3[1].name} );");
+            }
+            else
+            {
+                Console.WriteLine($"You Lost to {kid.e.op3[2].name} );");
+            }
+            kid.e.op3.RemoveRange(0, 3);
+        }
+        ReduceBoredom();
+    }
